b02eaf8480738ca481390e029913904147b57340
Skillkat <66493830+Skillkat@users.noreply.github.com>
2025-08-21 11:24:06 -0400

    cliente y delivery logeados

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,7 +1,9 @@
-module.exports = (roles) => (req, res, next) => {
-  if (roles.includes(req.session.role)) {
-    next();
-  } else {
-    res.redirect('/login');
-  }
-};
+    module.exports = (roles) => (req, res, next) => {
+        console.log('Role middleware called for roles:', roles, 'user role:', req.session.role);
+        if (req.session.role && roles.includes(req.session.role)) {
+          next();
+        } else {
+          console.log('Unauthorized role, redirecting to /auth/login');
+          res.redirect('/auth/login');
+        }
+      };
